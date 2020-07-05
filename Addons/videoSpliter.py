805603c8bf82363ea8c25d4e47fb09378e9b7caf
Canis Ignem <jonperezetxebarria@gmail.com>
2020-07-05 16:30:01 +0200

    Addons deleted

--- Addons/videoSpliter.py
+++
@@ -1,9 +0,0 @@
-from detecto import utils
-import argparse
-#import ImageResicer as IR
-
-
-utils.split_video('RightPeace.mp4', 'Frames/' , prefix = 'PeaceR', step_size= 4)
-#size = (800, 600)
-#IR.rescale_images('frames/', size)
-
